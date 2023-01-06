f5a534ca47e2674d1bc35afb949d12aa1d87b5d7
Wevesson Madson <104700348+WevessonMadson@users.noreply.github.com>
2023-01-06 19:44:24 -0300

    Reorganização do arquivo auth.js

--- src/auth/auth.js
+++ src/auth/auth.js
@@ -1,11 +1,14 @@
 import jwt from "jsonwebtoken";
 import * as dotenv from "dotenv";
+
 dotenv.config();
 
 export const getToken = (payload, time) => {
-    let acessToken = jwt.sign(payload, process.env.SECRET_TOKEN, { expiresIn: `${time}` })
+    let acessToken = jwt.sign(payload, process.env.SECRET_TOKEN, { expiresIn: `${time}` });
+    
     const { exp, iat } = jwt.verify(acessToken, process.env.SECRET_TOKEN);
     const expiresIn = exp - iat;
+    
     return { acessToken, type: "bearer", expiresIn }
 }
 
@@ -15,19 +18,22 @@ export function authorization(req, res, next) {
             res.status(401).json({ message: "Token required." });
         } else {
             let acessToken = req.headers['authorization'].split(' ')[1];
+            
             jwt.verify(acessToken, process.env.SECRET_TOKEN);
+            
             next();
         }
     } catch (error) {
         console.log(error.message);
+        
         switch (error.message) {
             case "jwt expired":
                 res.status(401).json({ message: "token expirado" });
                 break;
-            case "invalid signature": case "jwt signature is required": case "jwt must be provided":
+            case "invalid signature": case "jwt signature is required":
                 res.status(401).json({ message: "assinatura inválida" });
                 break;
-            case "invalid token": case "jwt malformed":
+            case "invalid token": case "jwt malformed": case "jwt must be provided":
                 res.status(401).json({ message: "token inválido" });
                 break;
             default:
