e0b6a56655c5d902c84670662796e2d734327c9b
Wevesson Madson <104700348+WevessonMadson@users.noreply.github.com>
2023-01-06 19:40:37 -0300

    Reorganização do arquivo index.js

--- src/index.js
+++ src/index.js
@@ -3,16 +3,16 @@ import { Router } from "express";
 import booksRouters from "./routes/books_routers.js";
 import usersRouters from "./routes/users_routers.js";
 
+const app = express();
 const routes = Router();
 
 //Rota raiz
 routes.post('/', (req, res) => {
     res.status(200).json({ message: "Bem vindo a nossa API!" })
-})
+});
 
 routes.use(usersRouters, booksRouters);
 
-const app = express();
 app.use(express.json());
 app.use(routes);
 
