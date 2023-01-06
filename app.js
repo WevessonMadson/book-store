9a17aa438eec3b6e760f441aaa0a8bbdd2cedb8d
Wevesson Madson <104700348+WevessonMadson@users.noreply.github.com>
2023-01-06 19:37:25 -0300

    Reorganização do código do app.js

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
-import { startDB } from "./src/db/database.js";
-import app from "./src/index.js";
 import * as dotenv from "dotenv";
-const port = 3000;
+import app from "./src/index.js";
+import { startDB } from "./src/db/database.js";
 
 dotenv.config();
 startDB();
+
+const port = 3000;
+
 
 app.listen(port, () => console.log(`Aplicação rodando na porta ${port}`));
